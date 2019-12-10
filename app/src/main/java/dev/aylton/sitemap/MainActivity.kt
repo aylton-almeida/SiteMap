6b6ecc5ada9f7c01a736f6bb974be49a4c8632a0
Aylton Almeida <ayltonalmeida2@gmail.com>
2019-12-11 00:32:31 +0100

    '{startRepo}'

---
+++ app/src/main/java/dev/aylton/sitemap/MainActivity.kt
@@ -0,0 +1,19 @@
+package dev.aylton.sitemap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.aylton.sitemap.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
